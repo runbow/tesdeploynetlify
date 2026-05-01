d9624f2efd754dbd18aad8c47366418859b01a85
Rama Febriyan <rama006@brin.go.id>
2026-05-01 21:39:25 +0700

    edit app.py

--- api/app.py
+++ api/app.py
@@ -84,5 +84,5 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+    #app.run(debug=True)
